b0bea852f1b25b0c3b9b1ed4dbcc01b7583476bb
Suyash Adhikari <suyash@DESKTOP-VJERJ5B.>
2024-12-04 14:17:41 +0000

    ipynb code added

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements"""
 
 setup(
-    name='DimondPricePrediction',
+    name='DiamondPricePrediction',
     version='0.0.1',
     author='suyash-04',
     author_email='suyashadhikari04@gmail.com',
