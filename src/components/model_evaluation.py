78fbde2ded5bd218deb6e688f287f96f26aeef29
Suyash Adhikari <suyash@DESKTOP-VJERJ5B.>
2024-12-05 17:01:47 +0000

    structure updated

---
+++ src/components/model_evaluation.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from src.logger.my_logger import logging
+from dataclasses import dataclass
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pickle 
+from src.utils.utils import load_object
+from src.exception.exception import customexception
+
+@dataclass
+class ModelEvaluationConfig:
+    pass
+    
+    
+class ModelEvaluation:
+    def __init__(self):
+        pass
+    
+    
+    def initiate_model_evaluation(self):
+        try:
+            pass
+        except Exception as e:
+            logging.info()
+            raise customexception(e, sys)
+    
---
+++ src/components/model_trainer.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from src.logger.my_logger import logging
+from src.exception.exception import customexception
+import os
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+
+
+from src.utils.utils import save_object , evaluate_model
+from sklearn.linear_model import LinearRegression , Ridge , Lasso
+
+
+
+
+@dataclass
+class ModelTrainerConfig:
+    pass
+    
+    
+class ModelTrainer:
+    def __init__(self):
+        pass
+    
+    
+    def initiate_model_training(self):
+        try:
+            pass
+        except Exception as e:
+            logging.info()
+            raise customexception(e, sys)
+    
