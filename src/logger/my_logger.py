e6237be6198df4570b470303f9a762fbf8614ec6
Suyash Adhikari <suyash@DESKTOP-VJERJ5B.>
2024-12-05 16:14:28 +0000

    my_logger updated:

--- src/logger/my_logger.py
+++ src/logger/my_logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_path=os.path.join(os.getcwd(),"logs")
@@ -15,4 +16,4 @@ logging.basicConfig(level=logging.INFO,
                     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
                     
 )
-        
+            #[2024-01-10 15:57:26,997] 6 root - INFO -  this my second tesgting
