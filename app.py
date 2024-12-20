e0419a83e1414c1b958ba6710f0526bb402efc79
Suyash Adhikari <suyash@DESKTOP-VJERJ5B.>
2024-12-20 18:35:49 +0000

    code updated

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def predict_datapoint():
 
 # Execution begins here
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
